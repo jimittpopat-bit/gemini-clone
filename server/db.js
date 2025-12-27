f8b44211321382e4a7a19522b7c3c6e2a109aa12
jimittpopat-bit <jimittpopat@gmail.com>
2025-12-27 09:26:14 +0530

    fix: use DATABASE_URL for postgres on render

--- server/db.js
+++ server/db.js
@@ -1,21 +1,12 @@
-require("dotenv").config();
+
 
 const { Pool } = require("pg");
 
 const pool = new Pool({
- user: process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-host: process.env.DB_HOST,
-database: process.env.DB_NAME,
-port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
-
-pool.query("SELECT NOW()", (err, res) => {
-  if (err) {
-    console.error("DB connection error:", err);
-  } else {
-    console.log("DB connected at:", res.rows[0]);
-  }
-});
